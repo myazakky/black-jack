2496b4285602b3380f3e0d24dc272317e80d6d65
nimuC <nimuC@outlook.com>
2019-12-01 16:05:48 +0900

    refactor: make better CUI

--- black_jack.rb
+++ black_jack.rb
@@ -15,27 +15,29 @@ class BlackJack
     @cards.shuffle!
 
     player_drew_cards = @cards.draw(2)
-    puts "You drew #{player_drew_cards.deck_to_a}\n"
+    puts "You drew #{player_drew_cards.deck_to_s}\n"
     @player.add(player_drew_cards)
 
     dealer_drew_cards = @cards.draw(2)
-    puts "Dealer drew #{dealer_drew_cards.deck_to_a[0]}\n"
+    puts "Dealer drew #{dealer_drew_cards.deck_to_s}\n"
     @dealer.add(dealer_drew_cards)
 
     until @player.over?
+      puts "\nYou have #{@player.deck_to_s}"
       puts "total: #{@player.total}"
-      print 'stop? Y/n'
+      print 'stop? Y/n> '
       break if readline == "Y\n"
 
       drawn_cards = @cards.draw(1)
       @player.add(drawn_cards)
 
-      puts "You draw #{drawn_cards.deck_to_a}"
+      puts "You draw #{drawn_cards.deck_to_s}"
     end
 
-    @dealer.add(@cards.draw(1)) while @dealer.total < 17 unless @player.over?
+    @dealer.add(@cards.draw(1)) while @dealer.total < 17 || @player.over?
 
     puts <<~"EOS"
+
       Player: #{@player.total}
       Dealer: #{@dealer.total}
       Winner is #{judge}
@@ -49,8 +51,8 @@ class BlackJack
     player_difference = (21 - @player.total).abs
 
     case true
-    when @dealer.over? then 'player'
     when @player.over? then 'dealer'
+    when @dealer.over? then 'player'
     when dealer_difference == player_difference then 'none'
     when dealer_difference < player_difference then 'dealer'
     when dealer_difference > player_difference then 'player'
